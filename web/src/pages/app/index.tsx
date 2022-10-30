e584b6eee2feec65226206ed6a741e0ba7243e50
Gabriel Borel <biel_borel@hotmail.com>
2022-10-30 10:35:59 -0300

    Started to consume the GraphQL Gateway Service with ApolloClient, GraphQL-Codegen, Auth Setup and TailwindCSS Setup

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,17 +1,38 @@
-import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { withApollo } from '../../lib/withApollo';
+import {
+  getServerPageGetProducts,
+  ssrGetProducts,
+} from '../../graphql/generated/page';
+import { useMeQuery } from '../../graphql/generated/graphql';
 
-export default function Home() {
+const Home = ({ data }) => {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
+
+  console.log(data);
 
   return (
-    <div>
+    <div className='text-violet-500'>
       <h1>Hello World</h1>
+
+      <pre>me: {JSON.stringify(me, null, 2)}</pre>
+
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
 
       <pre>{JSON.stringify(user, null, 2)}</pre>
 
-      <a href="/api/auth/logout">Logout</a>
+      {/* <a href='/api/auth/logout'>Logout</a> */}
     </div>
   );
-}
+};
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {
+    // return await getServerPageGetProducts(null, ctx);
+
+    return { props: {} };
+  },
+});
+
+export default withApollo(ssrGetProducts.withPage()(Home));
